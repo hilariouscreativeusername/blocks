f9dd9c36ab78f0b9d52a7306cdbb6511dc1c387f
Jack <jabmilgate@live.com>
2022-06-23 17:25:21 +0100

    Begun macOS port

--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ import subprocess
 if __name__ == "__main__":
   if platform.system() == "Windows":
     subprocess.call("cmake -B build -G \"Visual Studio 16 2019\"")
+  elif platform.system() == "Darwin":
+    subprocess.call("cmake -B build -G \"Xcode\"", shell=True)
   else:
     print("Error: Unsupported operating system!")
