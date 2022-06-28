56750bfe6948d3a1d0d3bd84fddbe60cc3f03cc6
Jack <jabmilgate@live.com>
2022-06-28 18:12:09 +0100

    Add makefiles setup option

--- setup.py
+++ setup.py
@@ -6,5 +6,7 @@ if __name__ == "__main__":
     subprocess.call("cmake -B build -G \"Visual Studio 16 2019\"")
   elif platform.system() == "Darwin":
     subprocess.call("cmake -B build -G \"Xcode\"", shell=True)
+  elif platform.system() == "Linux":
+    subprocess.call("cmake -B build -G \"Unix Makefiles\"")
   else:
     print("Error: Unsupported operating system! \"", platform.system(), "\"")
